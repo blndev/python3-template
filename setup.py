3e0acaa2c561181561c85cdfdcce8a8ec260ad44
blndev <blndev@users.noreply.github.com>
2021-03-24 15:24:53 +0100

    add flask dependency at setup.py

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['prometheus_client'],
+    install_requires=['flask'],
 
     tests_require=['pytest', 'pytest-runner'],
 
